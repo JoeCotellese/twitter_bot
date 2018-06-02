69c219decadd7c2d94415965e8dd391ffa152b24
Joe Cotellese <joe@cotellese.net>
2018-06-02 09:55:41 -0400

    add a setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='twitter_bot',
+      version='0.1',
+      description='A bot to follow and unfollow people on twitter',
+      url='http://github.com/jcotellese/twitter_bot',
+      author='Joe Cotellese',
+      author_email='joe@cotellese.net',
+      license='MIT',
+      packages=['twitter_bot'],
+      zip_safe=False)
